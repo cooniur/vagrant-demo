e3a6e5f5e3e259eb168b937f622803487d4ea645
Tongliang Liu <tliu3@illumina.com>
2014-09-04 18:28:32 -0700

    Fixed bugs in tomcat role

--- ansible/roles/tomcat/templates/force-restart-tomcat.sh
+++ ansible/roles/tomcat/templates/force-restart-tomcat.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-pid=`ps -fe | grep '{{tomcat_dir}}/{{tomcat_version}}' | grep -v grep | tr -s ' ' | cut -d " " -f2`
+{{tomcat_dir}}/{{tomcat_version}}/bin/catalina.sh stop 10 -force
 
-if [ -n "$pid" ]; then 
-	{{tomcat_dir}}/{{tomcat_version}}/bin/catalina.sh stop 10 -force
-fi
-
-{{tomcat_dir}}/{{tomcat_version}}/bin/startup.sh
+{{tomcat_dir}}/{{tomcat_version}}/bin/catalina.sh start
--- ansible/roles/tomcat/templates/setenv.sh
+++ ansible/roles/tomcat/templates/setenv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export CATALINA_HOME="{{tomcat_dir}}/{{tomcat_version}}"
 export CATALINA_OUT="{{tomcat_log_dir}}/catalina.out"
 
 {% if tomcat_java_opts is defined %}
